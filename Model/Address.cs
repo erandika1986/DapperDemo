31b888f511c11131a2f25af96632f6a0487dc9d9
erandika1986@gmail.com <erandika1986@gmail.com>
2017-05-01 10:01:32 +0530

    SQL Script

--- Model/Address.cs
+++ Model/Address.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DapperDemo
+﻿namespace DapperDemo
 {
     public class Address
     {
